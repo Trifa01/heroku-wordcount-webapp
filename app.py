1e39c55a3c7010b339300f1d7ea9fb67378fea1e
Ahmed Trifa <atrifa@equancy.com>
2021-04-24 19:33:29 +0200

    another message changing

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-
+# https://realpython-example-app-atrifa.herokuapp.com/
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello--World"
+    return "This is yet another version"
 
 
 if __name__ == "__name__":
