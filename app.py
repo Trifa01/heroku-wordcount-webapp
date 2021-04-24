c21f494cfeddeaa8609e3c78856524699b4b38f6
Ahmed Trifa <atrifa@equancy.com>
2021-04-24 21:06:55 +0200

    add config support

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
-# https://realpython-example-app-atrifa.herokuapp.com/
-app = Flask(__name__)
+import os
 
+app = Flask(__name__)
+env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
 
 @app.route("/")
 def index():
-    return "This is yet another version"
+    secret_key = app.config.get("SECRET_KEY")
+    return f"The configured secret key is {secret_key}"
 
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     print('end script')
